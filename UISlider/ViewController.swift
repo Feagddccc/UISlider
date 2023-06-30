c725f5e1932e969de62d87f9e9aa6fc272fe852f
AndyP <firreatwill@mail.ru>
2023-06-30 17:41:26 +0300

    add to slider

--- UISlider/ViewController.swift
+++ UISlider/ViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ // MARK: - IBOutlet
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: - IBAction
 
 
 }
