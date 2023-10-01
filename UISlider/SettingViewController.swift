3294cb4f4ec285b7ec50577cae1e6d5ead2e51c3
AndyP <firreatwill@mail.ru>
2023-10-01 16:32:57 +0300

    keyboard cannot be implemented

--- UISlider/SettingViewController.swift
+++ UISlider/SettingViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class SettingViewController: UIViewController {
  // MARK: - IBOutlet
     @IBOutlet var colorView: UIView!
     
